b0428c8d29e56c629f1cd79cabcc78ea45a4d0c9
nicopcat <tianfang07@outlook.com>
2022-05-02 02:06:18 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-header></the-header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheHeader from "./components/layouts/TheHeader.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { TheHeader },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <div class="logo">
+      <a href="/">
+        <img src="/assets/logo-tomato.svg" />
+      </a>
+    </div>
+    <div class="menu">
+      <ul>
+        <li class="blog">Blog</li>
+        <li class="Proj">Projects</li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.logo {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+</style>
