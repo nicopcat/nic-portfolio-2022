9a003a1c430f65274103fb2b867f110c8ddd3487
nic <ying_12004@126.com>
2022-06-27 22:55:35 +0800

    ~~

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-const path = require('path')
 
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
@@ -11,15 +10,4 @@ module.exports = defineConfig({
     maskIcon: 'img/icons/safari-pinned-tab.svg',
     msTileImage: 'img/icons/msapplication-icon-144x144.png'
   },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': path.join(__dirname, './src'),
-        '@views': path.join(__dirname, './src/views'),
-        '@components': path.join(__dirname, './src/components'),
-        '@utils': path.join(__dirname, './src/utils'),
-        '@assets': path.join(__dirname, './src/assets')
-      }
-    }
-  }
 })
