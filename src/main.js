28d79df3db923471c858cba71e1b5447ade93872
nicopcat <tianfang07@outlook.com>
2022-06-28 22:28:19 +0800

    test

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from "./App.vue";
 import BaseButton from "./components/ui/BaseButton";
 import BaseWrapper from "./components/ui/BaseWrapper";
 import './assets/css/global.css'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 
 const app = createApp(App);
 app.component("base-button", BaseButton);
